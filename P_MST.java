6521da39d31a6b5d1128b66c41c7b56caff43374
Aaditya Bansal <142680400+aaditya-30@users.noreply.github.com>
2025-10-01 12:42:35 +0530

    Add files via upload

---
+++ P_MST.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+class Product{
+    int id;
+    int namecode;
+    double price;
+
+    public Product(int id, int nc, double price) {
+        this.id = id;
+        namecode = nc;
+        this.price = price;
+    }
+    void displayProductDetails()
+    {
+        System.out.println("Product ID: "+id+"\nName Code: "+namecode+"\nPrice: "+price);
+    }
+
+}
+public class P_MST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int id = sc.nextInt();
+        int namecode = sc.nextInt();
+        double price = sc.nextDouble();
+        Product pt = new Product(id, namecode, price);
+        pt.displayProductDetails();
+    }
+}
